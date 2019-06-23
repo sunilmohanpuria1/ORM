6b995837aeb2247191c308ae6a69988dff83a3e4
sunilmohanpuria1 <51696478+sunilmohanpuria1@users.noreply.github.com>
2019-06-23 23:24:07 +0530

    migrations

---
+++ 20190621193549-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      classId: {
+        type: Sequelize.INTEGER,
+	references: {
+    	model: 'UserClasses',
+   	 key: 'classId'
+      	},
+},
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
